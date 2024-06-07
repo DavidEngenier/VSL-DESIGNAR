78479e7c10284bfa2236cdf95f46fc961f90256e
DavidEngenier <swguillermo@gmail.com>
2024-06-07 14:22:17 -0600

    cosa2

--- app/(auth)/Pagina2/page.tsx
+++ app/(auth)/Pagina2/page.tsx
@@ -10,9 +10,9 @@ const Home = () => {
   return (
     <>
 
-<blockquote className="text-5xl sm:text-5xl md:text-6xl lg:text-7xl font-semibold text-center text-gray-200 mx-auto text-center relative uppercase  sm:pt-8 lg:pt-12">
+<blockquote className="text-2xl sm:text-2xl md:text-6xl lg:text-7xl font-semibold text-center text-gray-200 mx-auto text-center relative uppercase  sm:pt-8 lg:pt-12">
             SOLO POR
-            <span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-blue-800 relative inline-block uppercase mr-5 mt-5 ml-8  sm:r-5 mt-5 lg: mt-5 ml-5">
+            <span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-blue-800 relative inline-block uppercase mr-5 mt-5 ml-5  sm:r-5 mt-5 lg: mt-5 ml-5">
               <span className="relative text-white">hoy </span>
             </span>
             <span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-red-400 relative inline-block uppercase mr-5 mt-5 ml-8  sm:r-5 mt-5 lg: mt-5 ml-5">
@@ -20,11 +20,11 @@ const Home = () => {
               
             </span>
             <div className="flex">
-      <div className=" sm:mx-auto ml-[100px] lg:mx-auto xl:mx-auto 2xl:mx-auto " >
+      <div className=" sm:mx-auto ml-[100px] lg:mx-auto xl:mx-auto 2xl:mx-auto  " >
       <div style={{ transform: "scale(1.5)"}}>
         <Camion />
         </div>
-        <div className="flex sm:flex-row flex-col justify-center mt-[70px] ml-[50px] sm:mx-auto"  style={{ transform: "scale(2)"}}>
+        <div className="flex flex-row  mt-[50px] ml-[20px] sm:mx-auto"  style={{ transform: "scale(1.3)"}} >
        
         <MySvgfe/> <MySvgfeds/>
         
@@ -42,8 +42,8 @@ const Home = () => {
 
       <blockquote className=" sm:text-1xl md:text-2xl lg:text-3xl font-semibold text-center text-gray-200 mx-auto text-center relative uppercase  sm:pt-8 lg:pt-12">
             Agenda solo 
-            <span className="before:block  before:-inset-1 before:-skew-y-3 before:bg-blue-800 relative inline-block uppercase mr-5 mt-5 ml-8  sm:r-5 mt-2 lg: mt-5 ml-5">
-              <span className="relative text-white"> si estas dispuesto a invertir.</span>
+            <span className="before:block  before:-inset-1 before:-skew-y-3 before:bg-blue-800 relative inline-block uppercase mr-6 mt-5 mb-5 ml-8  sm:r-5 mt-2 lg: mt-5 ml-5">
+              <span className="relative text-white"> si estas dispuesto a invertir en tu negocio</span>
             </span>
           </blockquote> 
      
